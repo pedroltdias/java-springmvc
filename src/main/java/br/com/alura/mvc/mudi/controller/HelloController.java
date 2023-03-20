7907022ff7292a2b915fb4378da3c5b7f627f79e
Pedro Dias <pedroltdias@gmail.com>
2023-03-20 10:20:47 -0300

    Feat: using 'model' from spring

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,17 +1,16 @@
 package br.com.alura.mvc.mudi.controller;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(HttpServletRequest req) {
-        req.setAttribute("nome", "Mundo");
+    public String hello(Model model) {
+        model.addAttribute("nome", "Mundo");
 
         return "hello";
     }
