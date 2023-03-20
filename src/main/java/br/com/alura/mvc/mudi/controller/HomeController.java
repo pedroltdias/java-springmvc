3cd63b1daf04cbe04176e06ee04ba1cf5cf48a4c
Pedro Dias <pedroltdias@gmail.com>
2023-03-20 16:36:47 -0300

    feat: using modelandview on homecontroller

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,11 +18,12 @@ public class HomeController {
     private PedidoRepository pedidoRepository;
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public ModelAndView home(Model model) {
         List<Pedido> pedidos = pedidoRepository.findAll();
-        model.addAttribute("pedidos", pedidos);
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("pedidos", pedidos);
 
-        return "home";
+        return mv;
     }
 
 }
