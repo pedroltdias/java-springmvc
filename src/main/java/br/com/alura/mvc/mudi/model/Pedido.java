5e90536abaf638d0fa4ea369e035ed94dba97f1e
Pedro Dias <pedroltdias@gmail.com>
2023-03-20 10:59:17 -0300

    Feat: homeController, model pedido and page home

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,73 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataDeEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public Pedido() {
+    }
+
+    public Pedido(String nomeProduto, BigDecimal valorNegociado, LocalDate dataDeEntrega, String urlProduto, String urlImagem, String descricao) {
+        this.nomeProduto = nomeProduto;
+        this.valorNegociado = valorNegociado;
+        this.dataDeEntrega = dataDeEntrega;
+        this.urlProduto = urlProduto;
+        this.urlImagem = urlImagem;
+        this.descricao = descricao;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataDeEntrega() {
+        return dataDeEntrega;
+    }
+
+    public void setDataDeEntrega(LocalDate dataDeEntrega) {
+        this.dataDeEntrega = dataDeEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
