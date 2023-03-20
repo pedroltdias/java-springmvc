5199264720af8d6c4f2017f54c6ab1246c7869eb
Pedro Dias <pedroltdias@gmail.com>
2023-03-20 10:19:01 -0300

    Feat: hello controller and page with thymeleaft

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,13 +1,18 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest req) {
+        req.setAttribute("nome", "Mundo");
+
         return "hello";
     }
 
